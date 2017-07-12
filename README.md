5544e7f1db1f429cb1036841d4a2db0a7d22f0d8
Eric Delaney <eric.delaney@puppet.com>
2017-07-12 13:59:27 -0700

    (PA-1267) Update to understand reverts and update readme.md
    ticketmatch.rb now attempts to reconcile Revert messages by associating the
    revert with the parent commit (if possible)
    There is now a new "REVERT" section which is all unassociated reverts
    Updated the readme.md to explain what and how to use ticketmaster.rb

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # ticketmatch
-Simple utility script for jia &lt;-> git reconciliation
+Simple utility script for Jira &lt;-> git reconciliation
+
+This script can be used to reconcile the git commit messages and what
+is contained in Jira. It does the following:
+
+* Collects the list of commits between the 2 provided git revisions
+* Collects the Jira tickets for a project and the fixed version
+* Reconciles the git commit ticket tags with the Jira ticket states
+* Associates reverts with the parent commit if possible
+
+# What you need
+
+* A checkout of the git repo to match tickets against
+* The revisions to check between
+  * These can be tags, hashes, HEAD, etc. (see gitrevisions)
+* The Jira project name (example PUP)
+* The Jira fix version to look for (example "PUP 4.10.5")
+
+# How to read the output
+
+## The git commit section
+
+The output contains some basic sections which roll up git commits that are not
+associated with a Jira ticket listed in the git commit.
+
+```
+** DOCS
+** MAINT
+** PACKAGING
+** UNMARKED
+```
+
+A special section exists for commits that ticketmatch.rb has determined are a 
+revert, but it could not associate the revert commit to a specific git commit
+(based on the revert's description string)
+
+```
+** REVERT
+    7f663f732  Revert "Merge pull request #5738 from Iristyle/ticket/LTS-1.7/PUP-1980-fix-URI-escaping-for-UTF8"
+    7dede9921  Revert "Merge pull request #5964 from Iristyle/ticket/stable/PUP-1890-fix-duplicated-forge-query-parameter-encoding"
+```
+
+## Jira ticket numbers
+
+The output also contains Jira ticket names found from the the git commit messages
+and the Jira tickets tagged with the fixed version supplied:
+
+Tickets that are preceded by a `--` have an associated Jira ticket with the supplied
+fixed version.
+
+Tickets that are preceded by a `**` were listed in the git commit messages only
+
+The `R` in column 2 denotes that the git commit is a revert of the preceding commit
+
+```
+-- PUP-6660 (Closed)
+    268135c91  Remove `--no-use_cached_catalog` flag from `--test` flag
+** PUP-6675
+    8ba63052a  Use pipes instead of temporary files for Puppet exec
+ R  e97179356  Revert "(PUP-6675) Use pipes instead of temporary files for Puppet exec"
+    651936006  Use pipes instead of temporary files for Puppet exec
+ R  e97179356  Revert "(PUP-6675) Use pipes instead of temporary files for Puppet exec"
+    8cec657e2  Add exec test using large output
+```
+
+## Jira Ticket status
+
+There are 3 sections which try to convey which of the Jira tickets are not in
+the proper state. Each section will print a state for the section.
+```
+----- Git commits in Jira -----
+----- Unresolved Jira tickets not in git commits -----
+----- Unresolved Jira tickets found in git commits -----
+```
+
+# How to run ticketmatch
+
+```$ cd <git_project_directory>  ```
+
+checkout the branch you want to run against  
+
+```$ git checkout 5.0.x  ```
+
+Run ticketmatch.rb
+
+```
+$ ruby path/ticketmatch.rb  
+Enter Git From Rev: 4.10.2
+Enter Git To Rev: |master| 4.10.3
+Enter JIRA project: |PUP|
+Enter JIRA fix version: |PUP 4.10.3|
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100  2001  100  1897  100   104   7295    399 --:--:-- --:--:-- --:--:--  7324
+** MAINT
+    e8bc39dba  correct comment in CharacterEncoding::scrub
+-- PUP-7650 (Closed)
+    a75f3c10a  Remove dead code and unnecessary call to #downcase
+    089ae6286  Fix issue related to deserialization of types
+    6f0e92737  Ensure that Loader::TypedName always uses lower-case
+-- PUP-7666 (Closed)
+    3dfa1cdb3  Update puppet version to 4.10.3
+** REVERT
+    7f663f732  Revert "Merge pull request #5738 from Iristyle/ticket/LTS-1.7/PUP-1980-fix-URI-escaping-for-UTF8"
+    7dede9921  Revert "Merge pull request #5964 from Iristyle/ticket/stable/PUP-1890-fix-duplicated-forge-query-parameter-encoding"
+
+----- Git commits in Jira -----
+COMMIT TOKENS NOT FOUND IN JIRA (OR NOT WITH FIX VERSION OF PUP 4.10.3)
+REVERT 7f663f732
+REVERT 7dede9921
+
+----- Unresolved Jira tickets not in git commits -----
+ALL ISSUES WERE FOUND IN GIT
+
+----- Unresolved Jira tickets found in git commits -----
+ALL ISSUES WERE RESOLVED IN JIRA
+```
--- ticketmatch.rb
+++ ticketmatch.rb
@@ -4,90 +4,328 @@
 #
 # You must cd to the git repo for this to work.
 #
+
 require 'rubygems'
 require 'highline/import' # see https://github.com/JEG2/highline
 require 'json'
 
-git_from = ask('Enter Git From Rev: ')
-git_to = ask('Enter Git To Rev: ') { |q| q.default = 'master' }
+# store the basic information of a git log message
+class GitEntry
+  attr_reader :hash, :description
+  attr_accessor :revert_entry, :has_revert_parent
+
+  def initialize(hash, description)
+    @hash        = hash
+    @description = description
+    # The GitEntry that is a revert of this commit
+    @revert_entry = nil
+    # we set this in the revert entry so we can avoid printing it twice
+    @has_revert_parent = false
+  end
+
+  def <=>(another_entry)
+    [self.hash, self.description] <=> [another_entry.hash, another_entry.description]
+  end
+end
+
+# hold all the git commit log messages, but group them based on their ticket number or "type"
+class GitCommit
+  def initialize
+    @git_entries = Hash.new
+  end
+
+  def add_git_commit_line(line)
+    # is this a revert?
+    m = line.match(/^([0-9a-fA-F]+)\s+(Revert ".*")$/)
+    unless m.nil?
+      hash        = m[1]
+      description = m[2]
+      add_git_entry('REVERT', hash, description)
+      return
+    end
+    m = line.match(/^([0-9a-fA-F]+)\s+(?:\(([^\)]*)\))?(.*)$/)
+    if m[2].nil?
+      hash        = m[1]
+      description = m[3]
+      add_git_entry('UNMARKED', hash, description)
+    else
+      type        = m[2].upcase
+      hash        = m[1]
+      description = m[3].lstrip
+      add_git_entry(type, hash, description)
+    end
+  end
+
+  def add_git_entry(type, hash, description)
+    if @git_entries[type].nil?
+      @git_entries[type] = [GitEntry.new(hash, description)]
+    else
+      @git_entries[type] << GitEntry.new(hash, description)
+    end
+  end
+
+  def set_has_revert_parent_for_entry(entry)
+    @git_entries.each_key do |ticket|
+      @git_entries[ticket].length.times do |entry_offset|
+        if @git_entries[ticket][entry_offset] == entry
+          @git_entries[ticket][entry_offset].has_revert_parent = true
+        end
+      end
+    end
+  end
+
+  def find_commit_that_matches_description(description_pattern)
+    @git_entries.each_key do |ticket|
+      @git_entries[ticket].length.times do |entry_offset|
+        if @git_entries[ticket][entry_offset].description =~ description_pattern
+          return @git_entries[ticket][entry_offset]
+        end
+      end
+    end
+    return nil
+  end
+
+  # we have to look 2 ways for reverts to find them all
+  # 1 - look using the "(TICKET) <description>"
+  # 2 - look using the "Revert "<description>""
+  def associate_reverts
+    @git_entries.each_key do |ticket|
+      @git_entries[ticket].length.times do |entry_offset|
+        # look for reverts based on the ticket name and description string
+        description_pattern = /^Revert "\(#{ticket}\) #{Regexp.escape(@git_entries[ticket][entry_offset].description)}"$/
+        revert              = find_commit_that_matches_description(description_pattern)
+        unless revert.nil?
+          @git_entries[ticket][entry_offset].revert_entry = revert
+          set_has_revert_parent_for_entry(revert)
+        end
+
+        # look for reverts based on the Revert string using just the description
+        description_pattern = /^Revert "#{Regexp.escape(@git_entries[ticket][entry_offset].description)}"$/
+        revert              = find_commit_that_matches_description(description_pattern)
+        unless revert.nil?
+          @git_entries[ticket][entry_offset].revert_entry = revert
+          set_has_revert_parent_for_entry(revert)
+        end
+      end
+    end
+  end
+
+  def keys
+    @git_entries.keys
+  end
+
+  def [](key)
+    @git_entries[key]
+  end
+
+  def empty?
+    @git_entries.empty?
+  end
+
+  def inspect
+    string = ""
+    @git_entries.each_key do |key|
+      string += "{#{key}: #{@git_entries[key].inspect} }\n"
+    end
+    string
+  end
+end
+
+# the basic state of a jira ticket
+#
+class JiraState
+  attr_accessor :in_git
+  attr_reader :state
+
+  def initialize(state, in_git=0)
+    @state  = state
+    @in_git = in_git # This is a count, if its its non-zero then something was still checked in for this ticket
+  end
+end
+
+# A grouping of Jira tickets that apply for this matching session
+#
+class JiraTickets
+  def initialize
+    @tickets = Hash.new
+  end
+
+  def [](ticket)
+    @tickets[ticket]
+  end
+
+  def keys
+    @tickets.keys
+  end
+
+  def add_ticket(ticket, state, in_git=0)
+    @tickets[ticket] = JiraState.new(state, in_git)
+  end
+
+  def empty?
+    @tickets.empty?
+  end
+
+  def inspect
+    string = ""
+    @tickets.each_key do |key|
+      string += "{#{key}: #{@tickets[key].inspect} }\n"
+    end
+    string
+  end
+end
+
+
+# check if we are in a git tree or not
+#
+in_repo = %x{git rev-parse --is-inside-work-tree 2>/dev/null}
+unless in_repo.chomp == "true"
+  say('ERROR: Please run ticketmatch from a git repo directory')
+  exit(1)
+end
+
+git_from_rev = ask('Enter Git From Rev: ')
+git_to_rev   = ask('Enter Git To Rev: ') {|q| q.default = 'master'}
 
 # Get the log from git
 # process and store in a hash per user entered ticket reference
 #
-result = Hash.new {|h, k| h[k] = [] }
-a = %x{git log --no-merges --oneline #{git_from}..#{git_to}}
-a.each_line do |line|
-  m = line.match(/^([0-9a-fA-F]+)\s+(?:\(([^\)]*)\))?(.*)$/)
-  result[(m[2] || 'unmarked').upcase] << [m[1], m[3]]
+git_commits = GitCommit.new
+git_log     = %x{git log --no-merges --oneline #{git_from_rev}..#{git_to_rev}}
+git_log.each_line do |line|
+  git_commits.add_git_commit_line(line)
 end
-
-if result.empty?
-  say("No results found in git log for range #{git_from}..#{git_to}")
+if git_commits.empty?
+  say("No results found in git log for range #{git_from_rev}..#{git_to_rev}")
   exit(status=1)
 end
 
-jira_project = ask('Enter JIRA project: ') { |q| q.default = 'PUP' }
-jira_version = ask('Enter JIRA fix version: ')
+# associate the reverts
+#
+git_commits.associate_reverts
+
+# collect the Jira information
+#
+jira_project_name          = ask('Enter JIRA project: ') {|q| q.default = 'PUP'}
+jira_project_fixed_version = ask('Enter JIRA fix version: ') {|q| q.default = "#{jira_project_name} #{git_to_rev}"}
+
+# get the list of tickets from the JIRA project that contain the fixed version
 jira_data = {
-    :jql => "project = #{jira_project} AND fixVersion = \"#{jira_version}\" ORDER BY key",
+    :jql        => "project = #{jira_project_name} AND fixVersion = \"#{jira_project_fixed_version}\" ORDER BY key",
     :maxResults => -1,
-    :fields => ['status']
+    :fields     => ['status']
 }
 # Process file with Jira issues
-post_data = JSON.fast_generate(jira_data)
+jira_post_data = JSON.fast_generate(jira_data)
 begin
-  jira_issues = JSON.parse(%x{curl -X POST -H 'Content-Type: application/json' --data '#{post_data}' https://tickets.puppetlabs.com/rest/api/2/search})
+  jira_issues = JSON.parse(%x{curl -X POST -H 'Content-Type: application/json' --data '#{jira_post_data}' https://tickets.puppetlabs.com/rest/api/2/search})
 rescue
   say('Unable to obtain list of issues from JIRA')
   exit(status=1)
 end
 # puts JSON.pretty_unparse(jira_issues)
 
-known_issues = (jira_issues['issues'] || []).reduce({}) {|memo, i| memo[i['key']] = [:not_in_git, i['fields']['status']['name']]; memo}
-if known_issues.empty?
-  say("JIRA returned no results for project '#{jira_project}' and fix version '#{jira_version}'")
+if jira_issues['issues'].nil?
+  say("JIRA returned no results for project '#{jira_project_name}' and fix version '#{jira_project_fixed_version}'")
+  exit(status=1)
+end
+jira_tickets = JiraTickets.new
+jira_issues['issues'].each do |issue|
+  jira_tickets.add_ticket(issue['key'], issue['fields']['status']['name'], in_git=0)
+end
+if jira_tickets.empty?
+  say("JIRA returned no results for project '#{jira_project_name}' and fix version '#{jira_project_fixed_version}'")
   exit(status=1)
 end
 
 # Print list of issues sorted, for each show sha + comment after reference
 #
-result.keys.sort.each do |k|
-  resolution = known_issues[k]
-  if resolution.nil?
-    puts "** #{k.upcase}"
+git_commits.keys.sort.each do |ticket|
+  if jira_tickets[ticket].nil?
+    puts "** #{ticket.upcase}"
   else
-    puts "-- #{k.upcase} (#{resolution[1]})"
-    resolution[0] = :in_git
+    puts "-- #{ticket.upcase} (#{jira_tickets[ticket].state})"
   end
-  v = result[k]
-  v.each do | data |
-    puts "    #{data[0]}  #{data[1]}"
+  git_commits[ticket].each do |git_entry|
+    next if git_entry.has_revert_parent # skip this entry if its a revert with a parent, (the parent will print it)
+
+    # list this entry and any of its
+    puts "    #{git_entry.hash}  #{git_entry.description}"
+    revert_ticket = git_entry.revert_entry
+    until revert_ticket.nil?
+      puts " R  #{revert_ticket.hash}  #{revert_ticket.description}"
+      revert_ticket = revert_ticket.revert_entry
+    end
   end
 end
-puts "---"
 
-unknown_issues = result.keys.reject do |k|
-  ["MAINT", "DOC", "DOCS", "TRIVIAL", "PACKAGING", "UNMARKED"].include?(k) ||
-  known_issues.include?(k)
+# figure out what is in git or not based on taking reverts into account
+# we add +1 for the initial code and then -1, or +1 depending on the level of the revert
+# if the number is non-zero then this ticket had a code change
+#
+git_commits.keys.each do |ticket|
+  # if we have not jira ticket for this one move on
+  next if jira_tickets[ticket].nil?
+  git_commits[ticket].each do |git_entry|
+    # if this git_entry has a parent revert_ticket, let that one do the counting
+    next if git_entry.has_revert_parent
+
+    # add 1 for this ticket adding code
+    jira_tickets[ticket].in_git += 1
+
+    in_git        = -1
+    revert_ticket = git_entry.revert_entry
+    until revert_ticket.nil?
+      # now add either -1 or +1 depending on Revert level
+      jira_tickets[ticket].in_git += in_git
+      revert_ticket = revert_ticket.revert_entry
+      in_git        = -in_git
+    end
+  end
+end
+
+puts
+puts '----- Git commits in Jira -----'
+known_jira_tickets = jira_tickets.keys
+unknown_issues     = git_commits.keys.reject do |ticket|
+  known_jira_tickets.include?(ticket) || ["MAINT", "DOC", "DOCS", "TRIVIAL", "PACKAGING", "UNMARKED"].include?(ticket)
 end
 if !unknown_issues.empty?
-  say("<%= color('COMMIT TOKENS NOT FOUND IN JIRA (OR NOT WITH FIX VERSION OF #{jira_version})', RED) %>")
-  unknown_issues.sort.each{ |k| say("<%= color('#{k}', RED) %>")}
+  say("<%= color('COMMIT TOKENS NOT FOUND IN JIRA (OR NOT WITH FIX VERSION OF #{jira_project_fixed_version})', RED) %>")
+  unknown_issues.sort.each do |ticket|
+    if ticket == 'REVERT'
+      git_commits[ticket].each do |revert_ticket|
+        say("<%= color('REVERT #{revert_ticket.hash}', RED) %>")
+      end
+    else
+      say("<%= color('#{ticket}', RED) %>")
+    end
+  end
 else
   say("<%= color('ALL COMMIT TOKENS WERE FOUND IN JIRA', GREEN) %>")
 end
 
-unresolved = known_issues.reject {|k,v| v[1] == 'Resolved' || v[1] == 'Closed' }
-unresolved_not_in_git = unresolved.reject {|k,v| v[0] == :in_git}
+puts
+puts '----- Unresolved Jira tickets not in git commits -----'
+unresolved            = known_jira_tickets.reject do |ticket|
+  jira_tickets[ticket].state == 'Resolved' || jira_tickets[ticket].state == 'Closed'
+end
+unresolved_not_in_git = unresolved.reject {|ticket| jira_tickets[ticket].in_git >= 1}
 if !unresolved_not_in_git.empty?
   say("<%= color('UNRESOLVED ISSUES NOT FOUND IN GIT', RED) %>")
-  unresolved_not_in_git.each_pair { |k,v| say("<%= color('#{k} #{v[1]}', RED) %>")}
+  unresolved_not_in_git.each {|ticket| say("<%= color('#{ticket} #{jira_tickets[ticket].state}', RED) %>")}
 else
   say("<%= color('ALL ISSUES WERE FOUND IN GIT', GREEN) %>")
 end
 
-unresolved_in_git =  unresolved.select {|k,v| v[0] == :in_git}
+puts
+puts '----- Unresolved Jira tickets found in git commits -----'
+unresolved_in_git = unresolved.select {|ticket| jira_tickets[ticket].in_git >= 1}
 if !unresolved_in_git.empty?
   say("<%= color('UNRESOLVED ISSUES FOUND IN GIT', RED) %>")
-  unresolved_in_git.each_pair { |k,v| say("<%= color('#{k} #{v[1]}', RED) %>")}
+  unresolved_in_git.each {|ticket| say("<%= color('#{ticket} #{jira_tickets[ticket].state}', RED) %>")}
+else
+  say("<%= color('ALL ISSUES WERE RESOLVED IN JIRA', GREEN) %>")
 end
+
+exit 0
